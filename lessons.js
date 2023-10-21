246ce130b9c6511803de7285ec764919b7c91d23
Krish Kalvani <k4spidey@gmail.com>
2023-10-21 09:35:00 +0400

    lessons inside a js file, added a 2nd lesson

---
+++ lessons.js
@@ -0,0 +1,23 @@
+let webstore = new Vue({
+    el: '#app',
+    data: {
+        sitename: 'AfterSchool',
+        lesson: {
+            id: 1001,
+            image: "images/MathIcon.jpg",
+            subject: "Math",
+            location: "London",
+            price: 20,
+            spaces: 0
+
+        },
+        lesson2: {
+            id: 1002,
+            image: "images/bioIcon.png",
+            subject: "Biology",
+            location: "London",
+            price: 25,
+            spaces: 0
+        }
+    }
+})
