055be6a7fd1cd6dc4f3855773c1dc0b5c3769495
Krish Kalvani <k4spidey@gmail.com>
2023-10-24 16:47:46 +0400

    I added the lessons (only 2 right) using the v-for I changed the lessons js object array. I adjusted the CSS

--- lessons.js
+++ lessons.js
@@ -1,23 +1,32 @@
 let webstore = new Vue({
     el: '#app',
     data: {
-        sitename: 'AfterSchool',
-        lesson: {
-            id: 1001,
-            image: "images/MathIcon.jpg",
-            subject: "Math",
-            location: "London",
-            price: 20,
-            spaces: 5
-
+      sitename: 'AfterSchool',
+      lessons: [
+        {
+          id: 1001,
+          image: "images/MathIcon.jpg",
+          subject: "Math",
+          location: "London",
+          price: 20,
+          spaces: 5
         },
-        lesson2: {
-            id: 1002,
-            image: "images/bioIcon.png",
-            subject: "Biology",
-            location: "London",
-            price: 25,
-            spaces: 5
+        {
+          id: 1002,
+          image: "images/bioIcon.png",
+          subject: "Biology",
+          location: "London",
+          price: 25,
+          spaces: 5
         }
+        // Add more lessons here if needed
+      ]
+    },
+    methods: {
+      addToCart(lesson) {
+        // Implement your addToCart logic here
+        // You can access the selected lesson using the 'lesson' parameter
+        console.log("Added to cart:", lesson);
+      }
     }
-})
+  });
